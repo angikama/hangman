d3ea143ed296da62613df6f924d9bc5d5c88e67c
angikama <angela.akamara@gmail.com>
2022-10-03 14:16:16 +0100

    Replaces '_' of corresponding letter with the user's guess

--- milestone_3.py
+++ milestone_3.py
@@ -1,36 +1,37 @@
 # %%
 import random
-from turtle import position
+import string
 
 class Hangman:
 
     def __init__(self, word_list, num_lives=5):
-        self.word_list = "mango", "pineapple", "grapes", "cherry","banana"
+        self.word_list = ["mango", "pineapple", "grapes", "cherry","banana"]
         self.num_lives = num_lives
-        self.word = (random.choice(word_list))
+        self.word = (random.choice(self.word_list))
         self.word_guessed = ['_'] * len(self.word)
         self.num_letters = len(self.word)
         self.list_of_guesses = []
         pass
 
-
     def check_guess(self, guess):
         guess = guess.lower()
         if guess in self.word:  
             print(f"Good guess! {guess} is in the word.")
-            for letter in self.word:
-                if letter == guess: #Unsure of how to replace the corresponding "_" in the word_guessed with the guess
+            for x, letter in enumerate(self.word):
+                if guess == letter:
+                    self.word_guessed[x] = letter
+                    print(self.word_guessed)
             self.num_letters -= 1
         else:
             self.num_lives -= 1
-            print(f"Sorry,{self.guess} is not in the word.")
-            print(f"You have{self.num_lives} lives left.") 
-    self.list_of_guesses.append(guess) 
+            print(f"Sorry, {guess} is not in the word.")
+            print(f"You have {self.num_lives} lives left.") 
+        self.list_of_guesses.append(guess) 
 
 
-    def ask_for_input():
+    def ask_for_input(self):
         while True:
-            guess = input("Guess a single letter")
+            guess = input("Guess a single letter ")
             if (len(guess) > 1) or (guess.isdigit()):
                 print("Invalid letter. Please, enter a single alphabetical character")
             elif guess in self.list_of_guesses:
@@ -41,14 +42,16 @@ class Hangman:
 
 def play_game():
     word_list = ["mango", "pineapple", "grapes", "cherry","banana"]
-    game = Hangman(word_list, num_lives=5) #should I make lives equal something or not
+    game = Hangman(word_list, num_lives=5)
     while True:
         if game.num_lives == 0:
             print("You lost!")
         elif game.num_letters > 0:
             game.ask_for_input()
-        else: #else or elif
-            game.num_lives > 0 and game.num_letters == 0
+        else:
+            game.num_lives != 0 and game.num_letters == 0
             print("Congratulations! You have won the game!")
+
+play_game()
 
 # %%
